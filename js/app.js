f0c1bd8184fc6d5d3bf8a52b3348ba2792ba76b1
teofilo_nicolau <teofilonicolau157@gmail.com>
2024-10-21 16:11:04 -0300

    sexta

---
+++ js/app.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const swiper = new Swiper('.swiper', {
+        direction: 'horizontal',
+        loop: false,
+        pagination: {
+            el: '.swiper-pagination'
+        },
+
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev'
+        }
+    })
+
+    document.addEventListener('keydown', function (event) {
+        if(event.key === 'ArrowLeft') {
+            swiper.slidePrev()
+        } else if(event.key === 'ArrowRight'){
+            swiper.slideNext()
+        }
+    })
+})
