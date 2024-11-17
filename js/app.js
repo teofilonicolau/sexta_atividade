80364f981a8286ee41f09d0f7975751885778536
TEOFILO NICOLAU <teofilonicolau157@gmail.com>
2024-11-17 17:22:10 -0300

    subir

--- js/app.js
+++ js/app.js
@@ -21,11 +21,38 @@ document.addEventListener('DOMContentLoaded', function() {
     })
 })
 
-//Menu hamburguer
+// MENU HAMBURGUER
+class MobileNavBar {
+    constructor(mobileMenu, navList, navLinks) {
+        this.mobileMenu = document.querySelector(mobileMenu);
+        this.navList = document.querySelector(navList);  // Atualizado para .nav
+        this.navLinks = document.querySelector(navLinks);
+        this.activeClass = "active";
 
-const hamburger = document.getElementById('hamburger');
-        const nav = document.querySelector('.nav');
+        this.handleClick = this.handleClick.bind(this);
+    }
 
-        hamburger.addEventListener('click', () => {
-            nav.classList.toggle('active');
-        });
+    handleClick() {
+        this.navList.classList.toggle(this.activeClass);
+        this.mobileMenu.classList.toggle(this.activeClass);
+    }
+
+    addClickEvent() {
+        this.mobileMenu.addEventListener("click", this.handleClick);
+    }
+
+    init() {
+        if (this.mobileMenu) {
+            this.addClickEvent();
+        }
+        return this;
+    }
+}
+
+const mobileNavBar = new MobileNavBar(
+    ".mobile-menu",  // Não mudou
+    ".nav",           // Alterado para .nav, já que é o nome da classe no HTML
+    ".nav li"         // Continua o mesmo
+);
+
+mobileNavBar.init();
